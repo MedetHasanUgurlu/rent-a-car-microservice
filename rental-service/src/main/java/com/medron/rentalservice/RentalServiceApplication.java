bb637c18df3a0128411548ff02d44262c68e9f38
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-05-02 19:22:25 +0300

    rental service

---
+++ rental-service/src/main/java/com/medron/rentalservice/RentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medron.rentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalServiceApplication.class, args);
+	}
+
+}
---
+++ rental-service/src/test/java/com/medron/rentalservice/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medron.rentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
