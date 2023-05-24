2094f201203711d578320185f75c7eefd8a185ae
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-05-24 20:56:42 +0300

    config server initiated2.

---
+++ config-server/src/main/java/com/medron/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.medron.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
