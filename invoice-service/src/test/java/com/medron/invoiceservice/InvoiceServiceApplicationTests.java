b597170c273b0cb0c2e34a767d88e4a85e9d0e91
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-05-27 08:21:09 +0300

    invoice-service #4

---
+++ invoice-service/src/test/java/com/medron/invoiceservice/InvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medron.invoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
