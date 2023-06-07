17365233bd9f0c64f44e508db47e26f5acf62e07
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-06-07 23:24:22 +0300

    SecurityFilterChain is refactored.

--- common-package/src/main/java/com/medron/commonpackage/config/SecurityConfig.java
+++ common-package/src/main/java/com/medron/commonpackage/config/SecurityConfig.java
@@ -13,21 +13,40 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        var converter = new JwtAuthenticationConverter();
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
         converter.setJwtGrantedAuthoritiesConverter(new JwtRoleConverter());
 
-        http.cors().and().authorizeHttpRequests()
-                .requestMatchers("/filter","api/v1/car/check-car-available/**","/api/v1/car/car-feature/**","/payment/pay/**","/api/v1/car/show-state/**")
-                .permitAll()
-                .requestMatchers("/api/v1/car")
-                .hasAnyRole("user")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .csrf().disable()
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(converter);
+        /*
+        Starting from Spring Security 6.1 and Spring Boot 3.1.0 versions,
+        the Spring Security framework team recommends using the Lambda DSL style for configuring security for APIs, web paths, etc.
+        Consequently, they have deprecated a few methods within the framework.
+        These deprecated methods are planned to be removed in Spring Security 7, which is expected to be released in the next 2-3 years.
+        This timeframe allows all developers sufficient time for migrating their code.
+         */
+        http.cors().and().authorizeHttpRequests((request)-> {
+            try {
+                request
+                        .requestMatchers("/filter","api/v1/car/check-car-available/**","/api/v1/car/car-feature/**","/payment/pay/**","/api/v1/car/show-state/**").permitAll()
+                        .requestMatchers("/api/v1/car").hasAnyRole("user").anyRequest().authenticated()
+                        .and().csrf().disable()
+                        .oauth2ResourceServer().jwt().jwtAuthenticationConverter(converter);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+//        http.cors().and().authorizeHttpRequests()
+//                .requestMatchers("/filter","api/v1/car/check-car-available/**","/api/v1/car/car-feature/**","/payment/pay/**","/api/v1/car/show-state/**")
+//                .permitAll()
+//                .requestMatchers("/api/v1/car")
+//                .hasAnyRole("user")
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .csrf().disable()
+//                .oauth2ResourceServer()
+//                .jwt()
+//                .jwtAuthenticationConverter(converter);
 
         return http.build();
     }
